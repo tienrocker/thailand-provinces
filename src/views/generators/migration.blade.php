1a6d9101855f48ea57a0ebdebf449d1109330474
james <jamestranw88@gmail.com>
2018-09-16 19:27:02 +0700

    fix wrong data

--- src/views/generators/migration.blade.php
+++ src/views/generators/migration.blade.php
@@ -1,5 +1,6 @@
 <?php echo '<?php' ?>
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 
@@ -13,49 +14,58 @@ class CreateProvincesTable extends Migration
      */
     public function up()
     {
+        Schema::dropIfExists(\Config::get('thprovinces.sub_districts_table'));
+        Schema::dropIfExists(\Config::get('thprovinces.districts_table'));
+        Schema::dropIfExists(\Config::get('thprovinces.provinces_table'));
+        Schema::dropIfExists(\Config::get('thprovinces.geographies_table'));
+
         // Creates the geographies table
         Schema::create(\Config::get('thprovinces.geographies_table'), function (Blueprint $table) {
-            $table->integer('id')->index();
+            $table->integer('id')->primary();
             $table->string('name');
-
-            $table->primary('id');
         });
 
         // Creates the provinces table
         Schema::create(\Config::get('thprovinces.provinces_table'), function (Blueprint $table) {
-            $table->integer('id')->index();
-            $table->unsignedInteger('code');
+            $table->integer('id')->primary();
+            $table->integer('code');
             $table->string('name_th');
             $table->string('name_en');
-            $table->unsignedInteger('geography_id');
+            $table->integer('geography_id');
 
-            $table->primary('id');
-            $table->foreign('geography_id')->references('id')->on(\Config::get('thprovinces.geographies_table'));
+            $table->foreign('geography_id')
+                ->references('id')
+                ->on(\Config::get('thprovinces.geographies_table'))
+                ->onDelete('cascade');
         });
 
         // Creates the districts table
         Schema::create(\Config::get('thprovinces.districts_table'), function (Blueprint $table) {
 
-            $table->integer('id')->index();
-            $table->unsignedInteger('code');
+            $table->integer('id')->primary();
+            $table->integer('code');
             $table->string('name_th');
             $table->string('name_en');
-            $table->unsignedInteger('province_id');
+            $table->integer('province_id');
 
-            $table->primary('id');
-            $table->foreign('province_id')->references('id')->on(\Config::get('thprovinces.provinces_table'));
+            $table->foreign('province_id')
+                ->references('id')
+                ->on(\Config::get('thprovinces.provinces_table'))
+                ->onDelete('cascade');
         });
 
         // Creates the sub districts table
         Schema::create(\Config::get('thprovinces.sub_districts_table'), function (Blueprint $table) {
-            $table->integer('id')->index();
-            $table->unsignedInteger('zip_code');
+            $table->integer('id')->primary();
+            $table->integer('zip_code');
             $table->string('name_th');
             $table->string('name_en');
-            $table->unsignedInteger('district_id');
+            $table->integer('district_id');
 
-            $table->primary('id');
-            $table->foreign('district_id')->references('id')->on(\Config::get('thprovinces.districts_table'));
+            $table->foreign('district_id')
+                ->references('id')
+                ->on(\Config::get('thprovinces.districts_table'))
+                ->onDelete('cascade');
         });
     }
 
--- src/views/generators/seeder.blade.php
+++ src/views/generators/seeder.blade.php
@@ -1,5 +1,6 @@
 <?php echo '<?php' ?>
 
+
 use Tienrocker\ThProvinces\Provinces\Districts;
 use Tienrocker\ThProvinces\Provinces\Geographies;
 use Tienrocker\ThProvinces\Provinces\Provinces;
@@ -12,11 +13,12 @@ class ProvincesSeeder extends Seeder
     /**
      * Run the database seeds.
      *
-     * @return  void
+     * @return    void
      */
     public function run()
     {
 
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         if (Schema::hasTable(\Config::get('thprovinces.sub_districts_table')))
             DB::table(\Config::get('thprovinces.sub_districts_table'))->truncate();
         if (Schema::hasTable(\Config::get('thprovinces.districts_table')))
@@ -25,6 +27,7 @@ class ProvincesSeeder extends Seeder
             DB::table(\Config::get('thprovinces.provinces_table'))->truncate();
         if (Schema::hasTable(\Config::get('thprovinces.geographies_table')))
             DB::table(\Config::get('thprovinces.geographies_table'))->truncate();
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
         // Get all of the geographies
         $provinces = (new Geographies())->getList();
@@ -50,25 +53,35 @@ class ProvincesSeeder extends Seeder
         // Get all of the districts
         $provinces = (new Districts())->getList();
         foreach ($provinces as $province) {
-            DB::table(\Config::get('thprovinces.districts_table'))->insert([
-                'id' => $province['id'],
-                'code' => $province['code'],
-                'name_th' => $province['name_th'],
-                'name_en' => $province['name_en'],
-                'province_id' => $province['province_id']
-            ]);
+            try {
+                DB::table(\Config::get('thprovinces.districts_table'))->insert([
+                    'id' => $province['id'],
+                    'code' => $province['code'],
+                    'name_th' => $province['name_th'],
+                    'name_en' => $province['name_en'],
+                    'province_id' => $province['province_id']
+                ]);
+            } catch (\Exception $e) {
+                $this->command->info('Error data: ' . json_encode($province));
+                $this->command->error($e->getMessage());
+            }
         }
 
         // Get all of the provinces
         $provinces = (new SubDistricts())->getList();
         foreach ($provinces as $province) {
-            DB::table(\Config::get('thprovinces.sub_districts_table'))->insert([
-                'id' => $province['id'],
-                'zip_code' => $province['zip_code'],
-                'name_th' => $province['name_th'],
-                'name_en' => $province['name_en'],
-                'district_id' => $province['amphure_id']
-            ]);
+            try {
+                DB::table(\Config::get('thprovinces.sub_districts_table'))->insert([
+                    'id' => $province['id'],
+                    'zip_code' => $province['zip_code'],
+                    'name_th' => $province['name_th'],
+                    'name_en' => $province['name_en'],
+                    'district_id' => $province['amphure_id']
+                ]);
+            } catch (\Exception $e) {
+                $this->command->info('Error data: ' . json_encode($province));
+                $this->command->error($e->getMessage());
+            }
         }
     }
 }
